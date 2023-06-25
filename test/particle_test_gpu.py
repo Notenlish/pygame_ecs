bba366d352c7fb6de26aea0d643fe42ca3b5ec49
Notenlish <71970100+Notenlish@users.noreply.github.com>
2023-06-25 17:29:24 +0300

    Update particle_test_gpu.py

--- test/particle_test_gpu.py
+++ test/particle_test_gpu.py
@@ -5,7 +5,7 @@ from pygame._sdl2 import Renderer, Texture, Window, get_drivers
 
 pygame.init()
 
-ENTITY_AMOUNT = 5
+ENTITY_AMOUNT = 1_000 * 10
 WIDTH = 800
 HEIGHT = 600
 
@@ -39,11 +39,11 @@ class BallDrawSystem(pygame_ecs.BaseSystem):
     def update(self, entity_components):
         pos: Position = entity_components[Position]  # type: ignore
         ball_renderer: BallRenderer = entity_components[BallRenderer]  # type: ignore
-        
-        self.renderer.draw_color = ball_renderer.color  # type: ignore
-        self.renderer.clear()
 
-        self.texture.draw(None, (pos.x, pos.y, ball_renderer.radius, ball_renderer.radius))
+        self.texture.color = ball_renderer.color  # type: ignore
+        self.texture.draw(
+            None, (pos.x, pos.y, ball_renderer.radius, ball_renderer.radius)
+        )
 
 
 class BallPhysics(pygame_ecs.BaseSystem):
@@ -62,9 +62,10 @@ class BallPhysics(pygame_ecs.BaseSystem):
         if pos.y > HEIGHT or pos.y < 0:
             vel.vec.y *= -1
 
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 window = Window.from_display_module()
-renderer = Renderer(window)
+renderer = Renderer(window, accelerated=1)
 
 texture = Texture.from_surface(renderer, pygame.image.load("test/circle.png"))
 
@@ -85,12 +86,12 @@ for _ in range(ENTITY_AMOUNT):
         random.randint(0, WIDTH),
         random.randint(0, HEIGHT),
     )
-    radius = random.randint(5, 9)
-    color = [random.randint(0, 255) for _ in range(3)]
-    color.append(255)  # alpha
+    radius = random.randint(5, 15)
+    color = [255, 255, 255]
+    color.append(255)
     vel = pygame.math.Vector2(
-                (random.random() - 0.5) * 400 / 1000,
-                (random.random() - 0.5) * 400 / 1000,
+        (random.random() - 0.5) * 400 / 1000,
+        (random.random() - 0.5) * 400 / 1000,
     )
     entity = entity_manager.add_entity(component_manager)
     component_manager.add_component(entity, Position(center[0], center[1]))
@@ -110,6 +111,14 @@ while running:
 
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             running = False
+    renderer.draw_color = (
+        0,
+        0,
+        0,
+        255,
+    )  # type: ignore # renderer.draw_color is used for clearing the screen and drawing primitives
+    renderer.clear()
+
     ball_physics.dt = dt
     system_manager.update_entities(entities, component_manager, ball_physics)
     system_manager.update_entities(entities, component_manager, ball_draw_system)
